97403ea00479a48c02569cb2213950062766261f
xsp@feibo.cn <xsp@feibo.cn>
2017-08-10 17:01:30 +0800

    基础的webpack设置

--- app/index.js
+++ app/index.js
@@ -1,8 +1,15 @@
-console.log('dddd');
+import './app.css';
+import iconPlay from './asset/images/icon_play.png';
+import iconPlaying from './asset/images/paying.gif';
+import iconQrcod from './asset/images/qrcode.jpg';
+
 
 const test = () => {
-  console.log('ddd');
+  let img = document.createElement('img');
+  img.src = iconQrcod;
+  document.body.appendChild(img);
+  console.log(process.env.NODE_ENV === 'production', 'ddd');
   const a = [1,2,3];
-  console.log([...a,4]);
+  consdole.log([...a,4]);
 };
 test();
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,36 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+
+const debug = true;
+const extractSass = new ExtractTextPlugin({
+    filename: "[name].[contenthash].css"
+});
+
+const uglifyjs = new UglifyJsPlugin({
+  output: {
+    comments: false
+  },
+  compress: {
+    warnings: false
+  }
+});
+
+let plugin = [
+  new HtmlWebpackPlugin({
+    title: '主页',
+    template: path.resolve(__dirname, 'app/index.html')
+  }),
+  new CleanWebpackPlugin(['build']),
+  extractSass,
+  // uglifyjs,
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify('dev')
+  })
+]
+plugin = debug ? plugin : [...plugin, uglifyjs];
 
 module.exports = {
   entry: path.resolve(__dirname, 'app/index.js'),
@@ -13,27 +43,35 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        use: extractSass.extract({
+          use: [
+            'css-loader',
+            'sass-loader'
+          ],
+          fallback: 'style-loader'
+        })
       },
       {
         test: /\.js$/,
         include: path.resolve(__dirname, 'app'),
         use: ['babel-loader']
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
       }
     ]
   },
-  devtool: 'inline-source-map',
+  devtool: debug ? 'inline-source-map' : false,
   devServer: {
     host: '0.0.0.0'
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: '主页',
-      template: path.resolve(__dirname, 'app/index.html')
-    }),
-    new CleanWebpackPlugin(['build'])
-  ]
+  plugins: [...plugin]
 }
